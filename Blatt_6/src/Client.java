a0612f7e00d0e0336a53acbafeb9da403ffd0a55
hmPham97 <pham7@hm.edu>
2018-11-27 12:54:30 +0100

    update

--- Blatt_6/src/Client.java
+++ Blatt_6/src/Client.java
@@ -1,24 +1,94 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.Socket;
+import java.sql.Time;
+import java.util.Timer;
 
 public class Client {
-    public static String address = "localhost";
-    public static int port = 7999;
+    private static String address = "localhost";
+    private static int port = 7999;
+    private static long time = 0;
+    private static int endTime = 10;
+    private static double sendRate = 0;
+
+
+    private static boolean UDP = false;
+
+
+
+    //private int delay = Integer.parseInt(args[0]);
+    private static int length = 0;
+    private static long start;
+    //private int packetSent = Integer.parseInt(args[1]);
+    private static int sent = 0;
+    private static int sendToserver = 0;
+    static byte[] buf;
+
     public static void main(String[] args) {
-        int length = 0;
+        int delay = Integer.parseInt(args[0]);
+        int packetSent = Integer.parseInt(args[1]);
+        if (UDP) {
+            udpClient(delay, packetSent);
+        } else {
+            tcpClient(delay, packetSent);
+        }
+        sendRate = (length * 0.008) / time;
+        System.out.println("packets sent:" + sendToserver);
+        System.out.println("LENGHT = " + length);
+        System.out.println("Length in kbit: " + length * 0.008);
+        System.out.println("TIME USED: " + time);
+        System.out.println(sendRate + " in kbit/s");
+    }
+
+    public static void udpClient(int delay, int offset) {
+        start = System.currentTimeMillis();
         try {
-            for(int i = 0; i < 3; i++) {
+            while (time < endTime) {
                 DatagramSocket socket = new DatagramSocket();
-                byte[] buf = new byte[1000];
+                buf = new byte[1400];
                 InetAddress adr = InetAddress.getByName(address);
                 DatagramPacket packet = new DatagramPacket(buf, buf.length, adr, port);
                 length = length + buf.length;
-                System.out.println(length);
                 socket.send(packet);
+                sent++;
+                sendToserver++;
+                if (sent == offset) {
+                    sent = 0;
+                    Thread.sleep(delay);
+                }
+                time = (System.currentTimeMillis() - start) / 1000;
+
+                socket.close();
             }
-        } catch(IOException e) {
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void tcpClient(int delay, int offset) {
+        start = System.currentTimeMillis();
+        try {
+            InetAddress adr = InetAddress.getByName(address);
+            Socket tcpSocket = new Socket(adr, port);
+            BufferedOutputStream dOut = new BufferedOutputStream(tcpSocket.getOutputStream());
+            BufferedInputStream dIn = new BufferedInputStream(tcpSocket.getInputStream());
+            while (time < endTime) {
+                buf = new byte[1400];
+                dOut.write(buf);
+                dOut.flush();
+                length = length + buf.length;
+                sent++;
+                sendToserver++;
+                if (sent == offset) {
+                    sent = 0;
+                    Thread.sleep(delay);
+                }
+                time = (System.currentTimeMillis() - start) / 1000;
+            }
+            tcpSocket.close();
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
     }
--- Blatt_6/src/Server.java
+++ Blatt_6/src/Server.java
@@ -1,43 +1,101 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.SocketException;
+import java.net.*;
 
-public class Server extends Thread{
-    private DatagramSocket socket;
+public class Server extends Thread {
+
     private boolean running = true;
+    private boolean isUDP = false;
+
     private byte[] buf = new byte[1400];
-    private int size = 0;
-    public Server() {
+    private long size = 0;
+    private long start = 0;
+    private int received = 0;
+    private int port = 7999;
+    private long end;
+    private double dsize;
+    private double result;
+    private int timeout = 5000;
+
+    private BufferedOutputStream dOut;
+    private BufferedInputStream dIn;
+    private DatagramSocket socket;
+    private ServerSocket tcpServer;
+
+
+    private Server() {
         try {
-            socket = new DatagramSocket(7999);
+            if (isUDP) {
+                socket = new DatagramSocket(port);
+            } else {
+                tcpServer = new ServerSocket(port);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public void run() {
-        long start = 0; // oder System.nanotime() ?
-        //long end = 0;
+        if (isUDP) {
+            UDP();
+        } else {
+            TCP();
+        }
+    }
+
+    private void UDP() {
         try {
-            socket.setSoTimeout(5000); // set timeout
+            socket.setSoTimeout(timeout); // set timeout
             start = System.currentTimeMillis();
-            while(running) {
-                DatagramPacket packet = new DatagramPacket(buf, buf.length);
+            DatagramPacket packet = new DatagramPacket(buf, buf.length);
+            while (running) {
                 socket.receive(packet); // get package from client
+                received++;
                 size = size + packet.getLength(); // get its length
-                //end = (end + System.currentTimeMillis()) / 1000;
             }
         } catch (IOException e) {
             //e.printStackTrace();
         }
-        long end = System.currentTimeMillis() - 5000;
-        end = end - start;
-        end = end / 1000;
-        long result = size / end;
-        System.out.println(size);
-        System.out.println(end);
-        System.out.println(result);
+        end = System.currentTimeMillis() - timeout;
+        end = (end - start) / 1000;
+        dsize = size * 0.008;
+        result = dsize / end;
+        System.out.println("RECEIVED: " + received);
+        System.out.println("Size in byte: " + size);
+        System.out.println("SIZE IN kbit: " + dsize);
+        System.out.println("TIME in sec: " + end);
+        System.out.println(result + " in kbit/sec");
+    }
+
+    private void TCP() {
+        try {
+            Socket tcpSocket = tcpServer.accept();
+            start = System.currentTimeMillis();
+            System.out.println(received + "" + size);
+            dIn = new BufferedInputStream(tcpSocket.getInputStream());
+            dOut = new BufferedOutputStream(tcpSocket.getOutputStream());
+            while (running) {
+                tcpServer.setSoTimeout(timeout);
+                size = dIn.available() + size;
+                dIn.skip(dIn.available());
+                System.out.println(size);
+                dOut.write();
+                dOut.flush();
+            }
+            dIn.close();
+        } catch (IOException e) {
+            // e.printStackTrace();
+        }
+        end = System.currentTimeMillis() - timeout;
+        end = (end - start) / 1000;
+        dsize = size * 0.008;
+        result = dsize / end;
+        System.out.println("Size in byte: " + size);
+        System.out.println("SIZE IN kbit: " + dsize);
+        System.out.println("TIME in sec: " + end);
+        System.out.println(result + " in kbit/sec");
     }
 
     public static void main(String[] args) {
