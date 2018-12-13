6cbdb94a756f12ef924800c46ecbd29fa8f288ec
hmPham97 <pham7@hm.edu>
2018-12-13 13:03:55 +0100

    added private class transition to receiver and sender. main.java is an example how fsmwoman works

--- Aufgabe8/src/FileReceiver.java
+++ Aufgabe8/src/FileReceiver.java
@@ -3,6 +3,11 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.zip.CRC32;
+import java.util.zip.Checksum;
 
 public class FileReceiver {
 
@@ -11,7 +16,7 @@ public class FileReceiver {
     }
 
     enum Doing {
-        RECEIVE_FROM_SERVER, SEND_TO_SERVER;
+        RECEIVE_FROM_SERVER, SEND_TO_SERVER, TIMED_OUT;
     }
 
     private DatagramSocket socket;
@@ -22,9 +27,22 @@ public class FileReceiver {
     private byte[] buffer = new byte[1028];
     private State currentState;
 
+    private byte[] sequencenumber = new byte[4];
+    private byte[] check = new byte[4];
+    private byte[] fromSender;
+    private Checksum checksum;
+    private ByteBuffer byteBuffer;
+    private  Transition[][] transitions;
     public FileReceiver() {
         try {
+            currentState = State.WAIT_FOR_PACKET;
+            transitions = new Transition[State.values().length][Doing.values().length];
+            transitions[State.WAIT_FOR_PACKET.ordinal()][Doing.RECEIVE_FROM_SERVER.ordinal()] = new Got_A_Packet();
+            transitions[State.RECEIVED_FROM_SERVER.ordinal()][Doing.SEND_TO_SERVER.ordinal()] = new Send_An_ACK();
+            transitions[State.WAIT_FOR_PACKET.ordinal()][Doing.TIMED_OUT.ordinal()] = new Timed_Out();
             socket = new DatagramSocket(port);
+            checksum = new CRC32();
+
         } catch (IOException e) {
         }
     }
@@ -33,6 +51,19 @@ public class FileReceiver {
         try {
             fromServer = new DatagramPacket(buffer, length);
             socket.receive(fromServer);
+
+            check = Arrays.copyOfRange(fromServer.getData(), 4, 7);
+            fromSender = Arrays.copyOfRange(fromServer.getData(), 8, fromServer.getLength() - 1);
+            checksum.update(fromSender);
+            byteBuffer = ByteBuffer.allocate(4);
+            byteBuffer.putLong(checksum.getValue());
+            byte[] f = byteBuffer.array();
+            for(int i = 0; i < f.length; i++) {
+                if (check[i] != f[i]) {
+                    // SOMETHING IS WRONG!!!!!!!!!!!!!!
+                }
+            }
+
         } catch (IOException e) {
 
         }
@@ -40,6 +71,15 @@ public class FileReceiver {
 
 
 
+
+    public void process(Doing input) {
+        System.out.println("INFO Received " + input + " in state " + currentState);
+        Transition trans = transitions[currentState.ordinal()][input.ordinal()];
+        if (trans != null) {
+            currentState = trans.execute(input);
+        }
+        System.out.println("current state: " + currentState);
+    }
 
     abstract class Transition {
         abstract public State execute(Doing input);
